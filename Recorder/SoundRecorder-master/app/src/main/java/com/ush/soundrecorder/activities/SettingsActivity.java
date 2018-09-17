1eea36a16a9ca51194be42df8465779f462f2f8a
hukrma <subodhkumar321.1@gmail.com>
2018-09-17 15:32:26 +0530

    project added

---
+++ Recorder/SoundRecorder-master/app/src/main/java/com/ush/soundrecorder/activities/SettingsActivity.java
@@ -0,0 +1,37 @@
+package com.ush.soundrecorder.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.preference.PreferenceActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+
+import com.ush.soundrecorder.R;
+import com.ush.soundrecorder.fragments.SettingsFragment;
+
+
+public class SettingsActivity extends android.support.v7.app.ActionBarActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.action_settings);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+
+        getFragmen tManager()
+                .beginTransaction()
+                .replace(R.id.container, new SettingsFragment())
+                .commit();
+    }
+}
---
+++ Recorder/SoundRecorder-master/app/src/main/java/com/ush/soundrecorder/fragments/SettingsFragment.java
@@ -0,0 +1,36 @@
+package com.ush.soundrecorder.fragments;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+import com.ush.soundrecorder.BuildConfig;
+import com.ush.soundrecorder.MySharedPreferences;
+import com.ush.soundrecorder.R;
+import com.ush.soundrecorder.activities.SettingsActivity;
+
+/**
+ * Created by Daniel on 5/22/2017.
+ */
+
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+
+        Preference aboutPref = findPreference(getString(R.string.pref_about_key));
+        aboutPref.setSummary(getString(R.string.pref_about_desc, BuildConfig.VERSION_NAME));
+        aboutPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                LicensesFragment licensesFragment = new LicensesFragment();
+                licensesFragment.show(((SettingsActivity)getActivity()).getSupportFragmentManager().beginTransaction(), "dialog_licenses");
+                return true;
+            }
+        });
+    }
+}
